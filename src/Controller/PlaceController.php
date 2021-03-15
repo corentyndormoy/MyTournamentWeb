9059ce80da4e01fcb4de492d4d99078b272a5e2d
corentyndormoy <corentyn.dormoy@gmail.com>
2021-03-15 13:43:07 +0100

    Create Place

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -2,17 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Place;
+use App\Form\PlaceType;
+use App\Repository\PlaceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PlaceController extends AbstractController
 {
     #[Route('/place', name: 'place')]
-    public function index(): Response
+    /**
+     * Affiche la liste des lieux
+     * 
+     * @param PlaceRepository $placeRepository
+     * 
+     * @return Response
+     */
+    public function index(PlaceRepository $placeRepository): Response
     {
         return $this->render('place/index.html.twig', [
-            'controller_name' => 'PlaceController',
+            'places' => $placeRepository->findAll(),
+        ]);
+    }
+
+
+    #[Route('/place-create', name: 'place_create')]
+    /**
+     * Crée un lieu
+     * 
+     * @param Request $request
+     * 
+     * @return Response
+     */
+    public function create(Request $request): Response
+    {
+        $place = new Place();
+        $placeForm = $this->createForm(PlaceType::class, $place);
+
+        $placeForm->handleRequest($request);
+        if($placeForm->isSubmitted() && $placeForm->isValid()){
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($place);
+            $manager->flush();
+    
+            return $this->redirectToRoute('place');
+        }
+
+        return $this->render('place/create.html.twig', [
+            'placeForm' => $placeForm->createView(),
         ]);
     }
 }
