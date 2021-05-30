04c0044746210c9bfcdd96ea52e70e43aa046a5b
corentyndormoy <corentyn.dormoy@gmail.com>
2021-05-30 17:46:15 +0200

    Fix User table

--- migrations/Version20210528172913.php
+++ migrations/Version20210528172913.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210528172912 extends AbstractMigration
+final class Version20210528172913 extends AbstractMigration
 {
     public function getDescription(): string
     {
